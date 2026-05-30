796044bfe5d46dc73aac7939fd95f621ea8e3549
mzfr <github@mzfr.in>
2026-05-30 15:11:01 +0800

    Removed daemon-mode

--- daemon-mode/src/extension/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import { DiscordRelay } from "./relay.js";
-
-export default function registerDiscordRelay(pi: ExtensionAPI) {
-  const relay = new DiscordRelay(pi);
-  relay.register();
-}
